eae67ef1972f744065fbc13c0f7e229e79568ace
Joselaine Romão <55799037+joselainejrs@users.noreply.github.com>
2023-08-29 19:52:06 -0300

    parte da loja

---
+++ aula29_08/loja.py
@@ -0,0 +1,33 @@
+class Produto:
+    def __init__(self, nome, preco, qtdEmEstoque):
+        pass
+
+    def comprar(self):
+        pass
+
+    def reporEstoque(self):
+        pass
+
+    def getPreco(self):
+        pass
+
+    def setPreco(self, preco):
+        self.preco = preco
+
+    def getQtdEmEstoque(self):
+        pass
+
+    def setQtdEmEstoque(self, qtdEmEstoque):
+        self.qtdEmEstoque = qtdEmEstoque
+
+
+class loja:
+    def __init__(self):
+        pass
+
+    def adicionarProduto(self, Produto):
+        pass
+
+    def mostrarProduto():
+        pass
+
